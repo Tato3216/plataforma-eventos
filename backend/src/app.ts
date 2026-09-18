6e94dd0213db0ef269c61b050046fcdb7eb2f15d
Tato3216 <jsantos1815@gmail.com>
2026-09-18 17:41:04 -0600

    chore(backend): preparacion API para frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,8 +9,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
-        credentials: true,
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
     })
 );
 
